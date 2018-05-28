a53ef5d2f5b096450a20e1529612263f90041066
Michał Sieczkowski <mikee2509@gmail.com>
2018-05-28 20:29:24 +0200

    Insert implementation without adjust tree

--- src/main/scala/jps/RTree.scala
+++ src/main/scala/jps/RTree.scala
@@ -34,18 +34,50 @@ case class RTree[T](root: Node[T], size: Int, minEntries: Int, maxEntries: Int) 
         }
     }
 
-    def createLeafs(remainingChildren: Vector[Entry[T]], leftSeed: Entry[T], rightSeed: Entry[T]): (Leaf[T], Leaf[T]) = ???
+
+    def linearPickNext(remainingChildren: Vector[Entry[T]]): (Entry[T], Vector[Entry[T]]) = {
+        (remainingChildren.head, remainingChildren.tail)
+    }
+
+    def createLeaves(remainingChildren: Vector[Entry[T]], leftLeaf: Leaf[T], rightLeaf: Leaf[T]): (Leaf[T], Leaf[T]) = {
+        if (remainingChildren.isEmpty) {
+            return (leftLeaf, rightLeaf)
+        }
+        val (child, newRemaining) = linearPickNext(remainingChildren)
+        //TODO check this condition:
+        // if one group has so few entries that all remaining entries must be assigned to it in order for it to have
+        // minimum number of entries, assign them and stop
+
+        val leftEnlargement = leftLeaf.bound.enlargementToFit(child.bound)
+        val rightEnlargement = rightLeaf.bound.enlargementToFit(child.bound)
+        if (leftEnlargement > rightEnlargement) {
+            createLeaves(newRemaining, leftLeaf, rightLeaf :+ child)
+        } else if (leftEnlargement < rightEnlargement) {
+            createLeaves(newRemaining, leftLeaf :+ child, rightLeaf)
+        }
+
+        val leftArea = leftLeaf.bound.area()
+        val rightArea = rightLeaf.bound.area()
+        if (leftArea > rightArea) {
+            createLeaves(newRemaining, leftLeaf, rightLeaf :+ child)
+        } else if (leftArea < rightArea) {
+            createLeaves(newRemaining, leftLeaf :+ child, rightLeaf)
+        }
+
+        if (leftLeaf.children.size > rightLeaf.children.size) {
+            createLeaves(newRemaining, leftLeaf, rightLeaf :+ child)
+        } else {
+            createLeaves(newRemaining, leftLeaf :+ child, rightLeaf)
+        }
+    }
 
     def splitNode(leaf: Leaf[T]): (Leaf[T], Leaf[T]) = {
         val children: Vector[Entry[T]] = leaf.children
         val (leftSeed, rightSeed): (Entry[T], Entry[T]) = linearPickSeeds(children)
         val remainingChildren = children.filter(c => c != leftSeed && c != rightSeed)
-        //if all entries have been assigned, stop
-        //if one group has so few entries that all remaining entries must be assigned to it in order for it to have
-        //  minimum number of entries, assign them and stop
-
-        //        val leftLeaf = Leaf()
-        createLeafs(remainingChildren, leftSeed, rightSeed)
+        val leftLeaf = Leaf(leftSeed)
+        val rightLeaf = Leaf(rightSeed)
+        createLeaves(remainingChildren, leftLeaf, rightLeaf)
     }
 
 
@@ -94,8 +126,10 @@ case class RTree[T](root: Node[T], size: Int, minEntries: Int, maxEntries: Int) 
         if (xSeeds._1 > ySeeds._1) (xSeeds._2, xSeeds._3) else (ySeeds._2, ySeeds._3)
     }
 
-    def adjustTree(l: Node[T], ll: Option[Node[T]]): RTree[T] = {
-        RTree(l, l.children.size, minEntries, maxEntries)
+    def adjustTree(n: Node[T], nn: Option[Node[T]]): RTree[T] = {
+        if (n == root) {
+            RTree(n, n.children.size, minEntries, maxEntries)
+        }
     }
 
     def chooseSubtree(children: Vector[Node[T]], entry: Entry[T]): Node[T] = {
